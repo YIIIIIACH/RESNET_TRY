0c2eff0f663222c002557bd4a201dde1f94a5db8
YIIIIIACH <you@example.com>
2019-09-21 20:45:31 +0800

    kk

--- resnet_valid_plt.py
+++ resnet_valid_plt.py
@@ -12,195 +12,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # block the warning message on tensorf
 L2_WEIGHT_DECAY = 0.001
 BATCH_NORM_DECAY = 0.001
 BATCH_NORM_EPSILON = 0.005
-
-def identity_block(input_tensor, kernel_size, filters):
-    """The identity block is the block that has no conv layer at shortcut.
-    # Arguments
-        input_tensor: input tensor
-        kernel_size: default 3, the kernel size of
-            middle conv layer at main path
-        filters: list of integers, the filters of 3 conv layer at main path
-    """
-    filters1, filters2, filters3 = filters
-    if tf.keras.backend.image_data_format() == 'channels_last':
-        bn_axis = 3
-    else:
-        bn_axis = 1
- 
-    x = tf.keras.layers.Conv2D(filters1, (1, 1), use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(input_tensor)
- 
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
-    x = tf.keras.layers.Activation('relu')(x)
- 
-    x = tf.keras.layers.Conv2D(filters2, kernel_size,
-                      padding='same', use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)
- 
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
- 
-    x = tf.keras.layers.Activation('relu')(x)
- 
-    x = tf.keras.layers.Conv2D(filters3, (1, 1), use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)
- 
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
- 
-    x = tf.keras.layers.add([x, input_tensor])
-    x = tf.keras.layers.Activation('relu')(x)
-    return x
-
-#------------------------------------------------------------------------------------------------------------
-
-def conv_block(input_tensor, kernel_size, filters, strides=(2, 2)):
-    """A block that has a conv layer at shortcut.
-    # Arguments
-        input_tensor: input tensor
-        kernel_size: default 3, the kernel size of
-            middle conv layer at main path
-        filters: list of integers, the filters of 3 conv layer at main path
-        stage: integer, current stage label, used for generating layer names
-    # Returns
-        Output tensor for the block.
-    Note that from stage 3,
-    the second conv layer at main path is with strides=(2, 2)
-    And the shortcut should have strides=(2, 2) as well
-    """
- 
-    filters1, filters2, filters3 = filters
- 
-    if tf.keras.backend.image_data_format() == 'channels_last':
-        bn_axis = 3
-    else:
-        bn_axis = 1
- 
-    x = tf.keras.layers.Conv2D(filters1, (1, 1), use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(input_tensor)
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
-    x = tf.keras.layers.Activation('relu')(x)
- 
- 
-    x = tf.keras.layers.Conv2D(filters2, kernel_size, strides=strides, padding='same',
-                      use_bias=False, kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
-    x = tf.keras.layers.Activation('relu')(x)
- 
-    x = tf.keras.layers.Conv2D(filters3, (1, 1), use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
- 
-    shortcut = tf.keras.layers.Conv2D(filters3, (1, 1), strides=strides, use_bias=False,
-                             kernel_initializer='he_normal',
-                             kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(input_tensor)
-    shortcut = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                         momentum=BATCH_NORM_DECAY,
-                                         epsilon=BATCH_NORM_EPSILON)(shortcut)
- 
-    x = tf.keras.layers.add([x, shortcut])
-    x = tf.keras.layers.Activation('relu')(x)
-    return x
-
-#-------------------------------------------------------------------------------------------------------------
-
-def resnet50( input_shape):
-    img_input = tf.keras.layers.Input(shape=input_shape)
- 
-    if tf.keras.backend.image_data_format() == 'channels_first':
-        x = tf.keras.layers.Lambda(lambda x: tf.keras.backend.permute_dimensions(x, (0, 3, 1, 2)),
-                          name='transpose')(img_input)
-        bn_axis = 1
-    else:  # channels_last
-        x = img_input
-        bn_axis = 3
- 
-    # Conv1 (7x7,64,stride=2)
-    x = tf.keras.layers.ZeroPadding2D(padding=(3, 3))(x)
- 
-    x = tf.keras.layers.Conv2D(64, (7, 7),
-                      strides=(2, 2),
-                      padding='valid', use_bias=False,
-                      kernel_initializer='he_normal',
-                      kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)
-    x = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                  momentum=BATCH_NORM_DECAY,
-                                  epsilon=BATCH_NORM_EPSILON)(x)
-    x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.ZeroPadding2D(padding=(1, 1))(x)
- 
-    # 3x3 max pool,stride=2
-    x = tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
- 
-    # Conv2_x
- 
-    # 1×1, 64
-    # 3×3, 64
-    # 1×1, 256
- 
-    x = conv_block(x, 3, [64, 64, 256], strides=(1, 1))
-    x = identity_block(x, 3, [64, 64, 256])
-    x = identity_block(x, 3, [64, 64, 256])
- 
-    # Conv3_x
-    #
-    # 1×1, 128
-    # 3×3, 128
-    # 1×1, 512
- 
-    x = conv_block(x, 3, [128, 128, 512])
-    x = identity_block(x, 3, [128, 128, 512])
-    x = identity_block(x, 3, [128, 128, 512])
-    x = identity_block(x, 3, [128, 128, 512])
- 
-    # Conv4_x
-    # 1×1, 256
-    # 3×3, 256
-    # 1×1, 1024
-    x = conv_block(x, 3, [256, 256, 1024])
-    x = identity_block(x, 3, [256, 256, 1024])
-    x = identity_block(x, 3, [256, 256, 1024])
-    x = identity_block(x, 3, [256, 256, 1024])
-    x = identity_block(x, 3, [256, 256, 1024])
-    x = identity_block(x, 3, [256, 256, 1024])
- 
-    # 1×1, 512
-    # 3×3, 512
-    # 1×1, 2048
-    x = conv_block(x, 3, [512, 512, 2048])
-    x = identity_block(x, 3, [512, 512, 2048])
-    x = identity_block(x, 3, [512, 512, 2048])
- 
-    # average pool, 1000-d fc, softmax
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-
-    bbox = tf.keras.layers.Dense(  4,name='bbox', kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY),bias_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x)                                                 
-    landmark = tf.keras.layers.Dense(  10,name='landmark' , kernel_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY),bias_regularizer=tf.keras.regularizers.l2(L2_WEIGHT_DECAY))(x) 
-    # Create model.
-    return tf.keras.models.Model(inputs=[img_input], outputs=[bbox,landmark], name='resnet50')
+from resnet  import resnet50
 
 
-
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 data_root = pathlib.Path('/home/yiiiiiach/MTCNN_TRY/image_align_celeba/')
 
 ROOT_DIR = os.getcwd()
@@ -238,7 +52,7 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(0.001),
 #model.summary()
 
 #steps_per_epoch=tf.ceil(len(all_image_paths)/BATCH_SIZE).numpy()
-model.load_weights('resnet_40weight.h5')
+model.load_weights('weight_file/resnet_40weight.h5')
 rand_seed = random.randint(0,tuple_of_data)
 test_image_path = all_image_paths[rand_seed]                  # the file to be test
 list_img = []
@@ -263,10 +77,13 @@ print('test_image_path', test_image_path)
 print('bbox result =======>> :   ', bbox_result)
 print('lm result =======>> :   ', lm_result)
 
-bbox_result[0] = (bbox_result[0]/128)*178
-bbox_result[1] = (bbox_result[1]/128)*218
-bbox_result[2] = (bbox_result[2]/128)*178
-bbox_result[3] = (bbox_result[3]/128)*218
+from PIL import Image , ImageDraw
+im = Image.open(test_image_path) 
+
+bbox_result[0] = (bbox_result[0]/128)*im.size[0]
+bbox_result[1] = (bbox_result[1]/128)*im.size[1]
+bbox_result[2] = (bbox_result[2]/128)*im.size[0]
+bbox_result[3] = (bbox_result[3]/128)*im.size[1]
 for i in range(0,5):
 	lm_result[2*i] = (lm_result[2*i]/128)*178
 	lm_result[2*i+1] = (lm_result[2*i+1]/128)*218
@@ -278,20 +95,19 @@ with open('10000img.json', 'r') as f:
 	print('load json: ' ,data[rand_seed])
 	exact_bbox = 	data[rand_seed][1:5]
 print('exact_bbox' , exact_bbox)
-exact_bbox[0] = (exact_bbox[0]/128)*178
-exact_bbox[1] = (exact_bbox[1]/128)*218
-exact_bbox[2] = (exact_bbox[2]/128)*178
-exact_bbox[3] = (exact_bbox[3]/128)*218
+exact_bbox[0] = (exact_bbox[0]/128)*im.size[0]
+exact_bbox[1] = (exact_bbox[1]/128)*im.size[1]
+exact_bbox[2] = (exact_bbox[2]/128)*im.size[0]
+exact_bbox[3] = (exact_bbox[3]/128)*im.size[1]
 
 
-from PIL import Image , ImageDraw
-im = Image.open(test_image_path) 
+
 drawSurface = ImageDraw.Draw(im)
 d = 20
-drawSurface.line(( (exact_bbox[0],exact_bbox[1]) , (exact_bbox[0]+exact_bbox[2],exact_bbox[1]) , (exact_bbox[0]+exact_bbox[2],exact_bbox[1]+exact_bbox[3]) , (exact_bbox[0],exact_bbox[1]+exact_bbox[3]) , (exact_bbox[0],exact_bbox[1]) ),fill = (0,255,0), width = 5)
-drawSurface.line(( (bbox_result[0],bbox_result[1]) , (bbox_result[0]+bbox_result[2],bbox_result[1]) , (bbox_result[0]+bbox_result[2],bbox_result[1]+bbox_result[3]) , (bbox_result[0],bbox_result[1]+bbox_result[3]) , (bbox_result[0],bbox_result[1]) ),fill = (255,0,0), width = 5)
+drawSurface.line(( (exact_bbox[0],exact_bbox[1]) , (exact_bbox[0]+exact_bbox[2],exact_bbox[1]) , (exact_bbox[0]+exact_bbox[2],exact_bbox[1]+exact_bbox[3]) , (exact_bbox[0],exact_bbox[1]+exact_bbox[3]) , (exact_bbox[0],exact_bbox[1]) ),fill = (0,255,0), width = 3)
+drawSurface.line(( (bbox_result[0],bbox_result[1]) , (bbox_result[0]+bbox_result[2],bbox_result[1]) , (bbox_result[0]+bbox_result[2],bbox_result[1]+bbox_result[3]) , (bbox_result[0],bbox_result[1]+bbox_result[3]) , (bbox_result[0],bbox_result[1]) ),fill = (255,0,0), width = 3)
 '''
-for i in range(0,5):
+for i in range(0,5):                    # plt landmark on surface
 
 	drawSurface.ellipse((lm_result[2*i]+ d,lm_result[2*i+1]- d,lm_result[2*i]+ d,lm_result[2*i+1]-d),fill = (0, 255, 0))
 '''
